1b55e5f82c938a2e605377f587eeb33d8e22241b
elchinovzla <sonnyng.vzla@gmail.com>
2024-01-19 12:14:00 -0700

    solve question with a button to change a message

---
+++ src/testcases/question1/Question1.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import './Question1.css';
+
+const Question1 = () => {
+  const [isUserLogin, setIsUserLogin] = useState(false);
+
+  const handleLogin = (_event) => {
+    setIsUserLogin((currentValue) => !currentValue);
+  };
+
+  return (
+    <div>
+      <button onClick={handleLogin}>Login</button>
+      {isUserLogin ? <h1>Welcome User!</h1> : <h1>Please Log In</h1>}
+    </div>
+  );
+};
+
+export default Question1;
---
+++ src/testcases/question1/Question1.test.js
@@ -0,0 +1,24 @@
+import { render, fireEvent, screen } from '@testing-library/react';
+import Question1 from './Question1';
+
+test('renders initial login message', () => {
+  render(<Question1 />);
+  const initialWelcomeMessage = screen.getByText('Please Log In');
+  expect(initialWelcomeMessage).toBeVisible();
+});
+
+test('renders welcome user if the login button is clicked once', () => {
+  render(<Question1 />);
+
+  fireEvent.click(screen.getByText('Login'));
+  const loggedInMessage = screen.getByText('Welcome User!');
+  expect(loggedInMessage).toBeVisible();
+});
+
+test('redners initial login message again if login button is clickec twice', () => {
+  render(<Question1 />);
+  fireEvent.click(screen.getByText('Login'));
+  fireEvent.click(screen.getByText('Login'));
+  const loggedOutMessage = screen.getByText('Please Log In');
+  expect(loggedOutMessage).toBeVisible();
+});
